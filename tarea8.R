2a70dad7bdb0f27f5ec140d706cc3fc14231132b
Mauricio-Rubio <mau.r.h54@gmail.com>
2022-11-09 10:57:59 -0600

    Poisson Distribution

--- tarea8.R
+++ tarea8.R
@@ -2,15 +2,22 @@
 
 ## (a) Menos de 7 horas 
 a<-pnorm(7,10,2)
+print("Menos de 7 horas: ")
+print(a)
 ## (b) Entre 8 y 13 horas
 b<-pnorm(13,10,2)-pnorm(8,10,2)
+print("Entre 8 y 13 horas: ")
+print(b)
 
 # Suponga que el número de accidentes que ocurren al día en un tramo carretero es una variable aleatoria Poisson de parametro λ = 3
 
 ## (a) Calcule la probabilidad de que ocurran 2 o más accidentes en un día cualquiera.
-
-a<-1-ppois(1,3)
+a1<- 1-dpois(0, lambda = 3)-dpois(1, lambda = 3)
+print("2 o más accidentes en un día: ")
+print(a1)
 
 ## b) Conteste el inciso anterior bajo la suposición de que ha ocurrido al menos un accidente.
-
-b<-1-ppois(0,3)
+aux<- 1-dpois(0, lambda = 3)
+b1<- a1 / aux
+print("2 o más accidentes en un día habiendo ocurrido al menos un accidente: ")
+print(b1)
